d5e82a5905544d76bc33dd152a73277dc90dc3cc
Dimitar Kazakov <62236384+mitkokazakov@users.noreply.github.com>
2024-02-19 12:38:01 +0200

    Initial commit plus background images  and colors

---
+++ todo-app/src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import TodoContainer from "./TodoContainer";
+
+function App() {
+  return (
+    <div className=" w-full min-h-screen bg-[#181824]">
+       <header className=" head-img min-h-[200px] w-full">
+
+       </header>
+
+       <TodoContainer />
+    </div>
+  );
+}
+
+export default App;
